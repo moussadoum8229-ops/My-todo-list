9172eaa4b5878c39933912f1cbba36540b8e1e89
Moussa Doumbia <Moussadoum8229@gmail.com>
2026-04-24 01:28:19 +0200

    my first todo-lidt

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+function App() {
+
+
+  return (
+    <div className=" h-screen bg-slate-900">
+      <div className="max-w-4xl mx-auto pt-20 px-6 ">
+        <h1 className="text-3xl text-slate-100 mb-4"> la Todo List</h1>
+        <form className="mb-10" >
+          <label htmlFor="todo-item" className="text-slate-50"> Ajouter une chose a faire</label>
+          <input type="text" className="mt-1 bg-white text-black   block w-full rounded" />
+          <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-[115px]:">Ajouter</button>
+        </form>
+        <ul>
+
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
