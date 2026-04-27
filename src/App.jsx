5fe3f6eea91c3d702ddc5fd4f2f5c2dcd108ef0a
Moussa Doumbia <Moussadoum8229@gmail.com>
2026-04-28 01:55:04 +0200

    Last commit de la liste de tache le repas est pres

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,66 @@
-import { useState, useEffect } from "react";
-import { nanoid } from "nanoid"
-import Listitem from "./assets/Components/Listitem"
-
-function App() {
-  const [todosList, setTodosList] = useState([
-    { content: "item1", id: nanoid(2) },
-    { content: "item2", id: nanoid(2) },
-    { content: "item3", id: nanoid(2) },
-    { content: "item4", id: nanoid(2) },
-    { content: "item5", id: nanoid(2) },
+import React from 'react'
+import { useState } from 'react'
+import { nanoid } from 'nanoid'
+import Listitem from './assets/Components/Listitem'
 
 
+export default function App() {
+const [inputValue, setInputValue] = useState("")
 
-  
+  const [todos, setTodos] = useState([
+{ content : "item 1", id : nanoid()},
+{ content : "item 2", id : nanoid()},
+{ content : "item 3", id : nanoid()},
+{ content : "item 4", id : nanoid()}, ])
 
-  ]);
+{/*1. Fontion pour supprimer un item de la liste */}
 
-  const [todo, setTodo] = useState("")
+function deleteItem(id){
+  setTodos(todos.filter((item) => item.id !== id))
+}
 
-  function deleteTodo(id) {
-    setTodosList(todosList => todosList.filter(item => item.id !== id))
+{/* Fontion pour supprimer un item de la liste */}
 
-  }
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTodosList([...todosList, { content: todo, id: nanoid() }])
 
-  }
+{/* 2. Fontion pour ajouter un item à la liste */}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  setTodos([...todos, {id : nanoid(), content : inputValue}])
+  setInputValue("")
+
+
+
+}
+{/* Fontion pour ajouter un item à la liste */}
+
 
 
   return (
-    <div className=" h-screen bg-slate-900">
-      <div className="max-w-4xl mx-auto pt-20 px-6 ">
-        <h1 className="text-3xl text-slate-100 mb-4"> la Todo List</h1>
-        <form onSubmit={handleSubmit} className="mb-10" >
-          <label htmlFor="todo-item" className="text-slate-50"> Ajouter une chose a faire</label>
-          <input type="text" value={todo} onChange={ (e) => setTodo(e.target.value)} placeholder="Ajouter une chose a faire" className="mt-1 bg-white text-black   block w-full rounded" />
-          <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-[115px]:">Ajouter</button>
+    <div className='h-screen w-screen bg-slate-900 '>
+      <div className='max-w-4xl mx-auto pt-20 px-6'>
+        <h1 className='text-3xl text-slate-100'>La to-do liste</h1>
+
+
+        <form onSubmit={handleSubmit} className='mb-10' >
+          <label htmlFor="todo-item" className='text-slate-50'>Ajouter une chose a faire</label>
+
+          <input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} className='mt-1 bg-white block w-full rounded' placeholder='Ajouter un item' />
+
+          <button  className='bg-white rounded mt-4 py-2 px-2 min-w-28.75'> Ajouter</button>
+
+          <ul className='p-0 mt-10'>
+            {todos.length === 0 ? <p className='text-red-500'>Aucun item dans la liste</p> : <p className='text-green-500'>Voici les elements de la liste</p>}
+           
+            { todos.length > 0 && todos.map((item) => (
+              < Listitem key={item.id} itemData={item} deleteItem={deleteItem} />
+            ))}
+          </ul>
+          
+
         </form>
-        <ul>
-          {todosList.length === 0 && <p className="text-white">Aucune Tache pour le moment</p>}
 
-          {todosList.length  > 0 && todosList.map(item => (
-            <Listitem key={item.id} itemData={item} deleteTodo={deleteTodo} />
-          )
-
-          )}
-
-
-
-
-        </ul>
       </div>
     </div>
-
   )
 }
-
-
-export default App
--- src/assets/Components/Listitem.jsx
+++ src/assets/Components/Listitem.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-export default function Listitem({itemData, deleteTodo}) {
+export default function Listitem({itemData , deleteItem}) {
   return (
     <li className='p-2 bg-zinc-200 mb-2 rounded flex'>
         <span>{itemData.content}</span>
-        <button onClick={() =>deleteTodo(itemData.id)} className='ml-auto bg-red-500 w-6 h-6 rounded'>X</button>
+        <button onClick={() => deleteItem(itemData.id)} className='ml-auto bg-red-600 w-6 h-6 rounded text-zinc-200'>X</button>
+
     </li>
   )
 }
-
-
