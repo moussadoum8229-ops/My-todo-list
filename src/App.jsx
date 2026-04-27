3a649698a579e62dbc33cac9e7a1f77eebb8a615
Moussa Doumbia <Moussadoum8229@gmail.com>
2026-04-27 22:54:19 +0200

    Ajout de la fonctionnalité ajouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid"
 import Listitem from "./assets/Components/Listitem"
+
 function App() {
   const [todosList, setTodosList] = useState([
     { content: "item1", id: nanoid(2) },
@@ -8,36 +9,47 @@ function App() {
     { content: "item3", id: nanoid(2) },
     { content: "item4", id: nanoid(2) },
     { content: "item5", id: nanoid(2) },
-   
-   
+
+
+
+  
+
   ]);
 
-function deleteTodo(id){
-  setTodosList(todosList => todosList.filter(item => item.id !== id))
+  const [todo, setTodo] = useState("")
 
-}
+  function deleteTodo(id) {
+    setTodosList(todosList => todosList.filter(item => item.id !== id))
+
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodosList([...todosList, { content: todo, id: nanoid() }])
+
+  }
 
 
   return (
     <div className=" h-screen bg-slate-900">
       <div className="max-w-4xl mx-auto pt-20 px-6 ">
         <h1 className="text-3xl text-slate-100 mb-4"> la Todo List</h1>
-        <form className="mb-10" >
+        <form onSubmit={handleSubmit} className="mb-10" >
           <label htmlFor="todo-item" className="text-slate-50"> Ajouter une chose a faire</label>
-          <input type="text" className="mt-1 bg-white text-black   block w-full rounded" />
+          <input type="text" value={todo} onChange={ (e) => setTodo(e.target.value)} placeholder="Ajouter une chose a faire" className="mt-1 bg-white text-black   block w-full rounded" />
           <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-[115px]:">Ajouter</button>
         </form>
         <ul>
-          
-          {todosList.map(item => (
-            <Listitem key={item.id} itemData={item} deleteTodo={deleteTodo}  />
-          ) 
-          
+          {todosList.length === 0 && <p className="text-white">Aucune Tache pour le moment</p>}
+
+          {todosList.length  > 0 && todosList.map(item => (
+            <Listitem key={item.id} itemData={item} deleteTodo={deleteTodo} />
+          )
+
           )}
-          
 
 
-          
+
+
         </ul>
       </div>
     </div>
